f2c8207ea17c9fcd8187fe7f00f683800ed011b2
dpushkar26 <deorepushkar2305@gmail.com>
2024-09-19 22:55:39 +0530

    commiting changes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Hero from './assets/components/Hero'
+import Footer from './assets/components/Footer'
+import Blog from './assets/components/blog'
+import Navbar from './assets/components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <>
+
+    <Navbar/>
+    <Hero/>
+    
+    <Blog/> 
+    <Footer/>
+    </>
+  )
+}
+
+export default App
